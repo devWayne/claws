e5d69c3657016bccfe3bb333e31a9e649d59c244
Wayne <dev.wayne.ix@gmail.com>
2014-09-02 14:28:24 +0800

    mvc

--- app/app.js
+++ app/app.js
@@ -1,11 +1,29 @@
 var express = require('express'),
   bodyParser = require('body-parser');
+  var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
 
-var ajaxRouter=require('./route/ajax'),
-    testRouter=require('./route/test');
+var config = require('../config'),
+  ajaxRouter = require('./route/ajax'),
+  testRouter = require('./route/test');
 
 var app = module.exports = express();
 var router = express.Router();
+
+var openURL = function(url) {
+  switch (process.platform) {
+    case "darwin":
+      exec('open' + url);
+      console.log(url)
+      break;
+    case "win32":
+      exec('start ' + url);
+      break;
+    default:
+      spawn('xdg-open', [url]);
+  }
+  return;
+}
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
@@ -13,16 +31,18 @@ app.use(bodyParser.urlencoded());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
-ajaxRouter(router,app);
-testRouter(router,app);
+ajaxRouter(router, app);
+testRouter(router, app);
 
-router.get('/', function(req, res){
+router.get('/', function(req, res) {
   res.render('home', {
     title: 'Params'
   });
 });
+
 app.use(router);
 
-app.listen(3000, function() {
+app.listen(config.port, function() {
   console.log('server started');
-}); 
+  openURL(" http://" + config.host + ':' + config.port);
+});
--- config.js
+++ config.js
@@ -1,3 +1,6 @@
 var config = {
+	host:'127.0.0.1',
+	port:3000
 }
+
 module.exports = config;
